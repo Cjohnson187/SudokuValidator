f36f13afb2cdf36115e824bb9654f9c5866c1baa
chris <cjohn268@msudenver.edu>
2019-09-16 19:42:07 -0600

    rev

--- HW4/HW4_Source.c
+++ HW4/HW4_Source.c
@@ -5,10 +5,11 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #define num_child_threads 27
-//typedef int bool;
+typedef int bool;
 
-//#define TRUE 1
-//#define FALSE 0
+#define TRUE 1
+#define FALSE 0
+#define UNCHECKED 2
 
 char* file_name = "input.txt";
 FILE* filepoint;
@@ -16,9 +17,9 @@ FILE* filepoint;
 int sum[num_child_threads];
 int check[27] = { 0 };
 
-bool rows_bool[9] = { TRUE };
-bool columns_bool[9] = { TRUE };
-bool blocks_bool[9] = { TRUE };
+bool rows_bool[9] = { 2 };
+bool columns_bool[9] = { 2 };
+bool blocks_bool[9] = { 2 };
 
 int rows_2D[9][9] = { 0 };
 int columns_2D[9][9] = { 0 };
@@ -35,15 +36,15 @@ typedef struct {
 
 
 int sample_sudoku[9][9] = {
-	{4,3,5,2,6,9,7,8,1},
-	{6,8,2,4,7,1,4,9,3},
-	{1,9,7,8,3,4,5,6,2},
-	{8,2,6,1,9,5,3,4,7},
-	{3,7,4,6,8,2,9,1,5},
-	{9,5,1,7,4,3,6,2,8},
-	{5,1,9,3,2,6,8,7,4},
-	{2,4,8,9,5,7,1,3,6},
-	{7,6,3,4,1,8,2,5,9}
+	{1,2,3,4,5,6,7,8,9},
+	{9,8,7,6,5,4,3,2,1},
+	{0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,9},
+	{0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,7,0},
+	{0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0},
+	{0,0,0,6,0,0,0,0,0}
 };
 /*
 void* check_block(void* param) {
@@ -64,7 +65,7 @@ void* check_row(void* param) {
 	int current_column = data->column;
 
 
-}*/
+}
 
 void* check_column(void* param) {
 
@@ -75,16 +76,13 @@ void* check_column(void* param) {
 
 
 }
-
+*/
 void* load_sudoku() {
 
 	int ch;
 	FILE* fptr;
 	fptr = fopen("input.txt", "r");
-	if (!fptr) {
-		printf("file not found");
-		return 0;
-	}
+	if (!fptr) return 1;
 
 	int i = 0;
 	int j = 0;
@@ -114,50 +112,9 @@ void* fill_v() {
 	for (i = 0; i < 9; i++) {
 		for (j = 0; j < 9; j++) {
 			columns_2D[i][j] = rows_2D[j][i];
+
 		}
 	}
-}
-
-void* fill_block() {
-
-
-}
-
-void* check_dup(int list[9][9]) {
-	int standard_array[9] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-	int i = 0;
-	int j = 0;
-	int x = 0;
-	int temp = 0;
-	int occurences = 0;
-	
-
-	/*
-	for (i = 0; i < 9; i++) {
-		temp = list[x][i];
-
-
-		for (j = 0; j < 9; j++) {
-			if (temp == list[x][j]) occurences++;
-		}
-		if (occurences > 1) {
-			bool_list[x] = FALSE;
-			x++;
-			i = 0;
-		}
-		if (i == 8) {
-			x++;
-			i = 0;
-			bool_list[x] = TRUE;
-		}
-		if (x == 8) {
-			break;
-		}
-	}*/
-
-
-
-
 }
 
 
